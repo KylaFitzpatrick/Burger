bfb23316ea6867cb1542eaca8627efdd0a806937
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-28 12:30:04 -0700

    adding jsquery for post and refactor query

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,9 +18,8 @@ router.get("/", function(req, res) {
     ], [
       req.body.burger_name
     ], function(result) {
-      // Send back the ID of the new quote
-    //   res.json({ id: result.insertId });
-      res.redirect("/");
+      // Send back the ID 
+      res.json({ id: result.insertId });
     });
   });
   
@@ -32,12 +31,6 @@ router.get("/", function(req, res) {
     burger.updateOne({
       devoured: true
     }, condition, function(result) {
-    //   if (result.changedRows == 0) {
-    //     // If no rows were changed, then the ID must not exist, so 404
-    //     return res.status(404).end();
-    //   } else {
-    //     res.status(200).end();
-    //   }
       res.json(result) 
     });
   });
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -10,3 +10,23 @@ $.ajax({
 console.log(error)
 });
 })
+
+$(".addBurger").on("click", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = 
+    {
+      burger_name: $("#bu").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers/", {
+     data: newBurger,
+      type: "POST"
+    }).then(function(data){
+        location.reload()
+    }).catch(function(error){
+    console.log(error)
+    });
+  });
