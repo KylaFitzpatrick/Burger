865aa073a43e05dd1023cdf3c8127615dd4f6759
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-30 11:04:01 -0700

    adding html and  css

---
+++ config.js
@@ -0,0 +1,15 @@
+
+  module.exports.databaseOptions = {
+      host: "localhost",
+  
+      // Your port; if not 3306
+      port: 3306,
+  
+      // Your username
+      user: "root",
+  
+      // Your password
+      password: "Kombuch@123",
+      database: "burgers_db"
+    };
+    
