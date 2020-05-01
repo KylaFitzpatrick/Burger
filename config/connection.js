23b20ac034e581e1dfcf914d444e5ca10939c298
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-30 17:18:44 -0700

    adding config

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,30 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-}else{
-  connection = mysql.createConnection(config.databaseOptions);
+} else {
+  // connection = mysql.createConnection(config.databaseOptions);
+  connection = mysql.createConnection({
+  host: "localhost",
+  
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "burgers_db"
+})
 }
 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-connection.connect();
+// connection.connect();
 module.exports = connection;
